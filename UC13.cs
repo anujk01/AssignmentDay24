e126f31b373c53cc9c7d5beb4ea720d71f4c4d16
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 19:10:44 +0530

    Refactor Code

---
+++ UC13.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    class UC13
+    {
+        public Func<string, bool> isValidFirstName = firstname => Regex.IsMatch(firstname, firstnameRegex);
+
+
+        public Func<string, bool> isValidLastName = lastname => Regex.IsMatch(lastname, lastnameRegex);
+
+
+        public Func<string, bool> isValidEmail = email => Regex.IsMatch(email, EmailRegex);
+
+
+        public Func<string, bool> isValidMobileNumber = mobilenumber => Regex.IsMatch(mobilenumber, NumberRegex);
+
+
+        public Func<string, bool> isValidPassword = password => Regex.IsMatch(password, PasswordRegex) && Regex.IsMatch(password, PasswordRegex);
+    }
+}
