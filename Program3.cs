e8fa86e5e869f152e87b5c18c147abede098af3f
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 18:31:52 +0530

    User Registration

---
+++ Program3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssignmentDay24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Email ");
+            string email = Console.ReadLine();
+            UC3 emailRegex = new UC3();
+
+            if (emailRegex.ValidEmail(email))
+                Console.WriteLine("Email is Correct");
+            else
+                Console.WriteLine("Email is Incorrect");
+        }
+    }
+}
---
+++ UC3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    class UC3
+    {
+        public static string EmailRegex = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
+        public bool ValidEmail(string email)
+        {
+            return Regex.IsMatch(email, EmailRegex);
+        }
+    }
+}
