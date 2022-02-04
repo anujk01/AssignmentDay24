f80f472829c8cbca71acf7b685aac89a9f01eabb
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 18:39:32 +0530

    User Registration

---
+++ UC6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    class UC6
+    {
+        public static string PasswordRegex = "^(?=.*?[A-Z])[A-Za-z0-9]{8,}$";
+
+        public bool ValidPassword(string password)
+        {
+            return Regex.IsMatch(password, PasswordRegex);
+        }
+    }
+}
