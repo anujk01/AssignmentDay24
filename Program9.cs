1d9e14b5e0162f7cdba23c42f2e13a4bb1db7bf1
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 18:49:08 +0530

    User Registration

---
+++ Program9.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssignmentDay24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Sample Email ");
+            string emailSample = Console.ReadLine();
+            UC9 EmailSampleRegex = new UC9();
+
+            if (EmailSampleRegex.ClearEmailSample(emailSample))
+                Console.WriteLine("Email is Correct");
+            else
+                Console.WriteLine("Email is Incorrect");
+        }
+    }
+}
---
+++ UC9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    class UC9
+    {
+        public static string SampleEmailRegex= "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+
+        public bool ClearEmailSample(string sampleEmail)
+        {
+            return Regex.IsMatch(sampleEmail, SampleEmailRegex); ;
+        }
+    }
+}
