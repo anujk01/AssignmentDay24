765f7af8c41d48b27f4efd4120ac15fdfd3cab24
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 19:01:28 +0530

    JUnit Test

---
+++ UC10.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay24
+{
+    public class UC10
+    {
+        private string message;
+        public UC10(string message)
+        {
+            this.message = message;
+        }
+
+        public string FirstName()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry for First Name Successfull";
+            else
+                return "Entry for First Name UnSuccessfull";
+        }
+
+        public string LastName()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry for Last Name Successfull";
+            else
+                return "Entry for Last Name UnSuccessfull";
+        }
+
+        public string Email()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry for Email Successfull";
+            else
+                return "Entry for Email UnSuccessfull";
+        }
+
+        public string MobileNumber()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry for Mobile Number Successfull";
+            else
+                return "Entry for Mobile Number UnSuccessfull";
+        }
+
+        public string Password()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry for Password Successfull";
+            else
+                return "Entry for Password UnSuccessfull";
+        }
+    }
+}
