eb680fb8d1aa60701b8eec8e0c427632c8393494
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 19:05:05 +0530

    JUnit Test

---
+++ UC11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    class UC11
+    {
+        public UC11()
+        {
+            UC11 Obj = new UC11();
+
+            public string MultipleEmails(string e1, string e2)
+            {
+                bool b1 = Obj.ValidEmail(e1);
+
+                bool b2 = Obj.ValidEmail(e2);
+
+                if (b1 && b2)
+                    return "Entry Successful";
+                else
+                    return "Entry Unsuccessful";
+
+            }
+        }
+    }
+}
