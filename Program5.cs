a10d9b27d6dd0cff0ec8fc563d74ad2468fe13f8
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 18:37:36 +0530

    User Registration

---
+++ Program5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssignmentDay24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Password ");
+            string password = Console.ReadLine();
+            UC5 passwordRegex = new UC5();
+
+            if (passwordRegex.ValidPassword(password))
+                Console.WriteLine("Password is Correct");
+            else
+                Console.WriteLine("Password is Incorrect");
+        }
+    }
+}
---
+++ UC5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    class UC5
+    {
+        public static string PasswordRegex = "^[a-z0-9]{8,}$";
+
+        public bool ValidPassword(string password)
+        {
+            return Regex.IsMatch(password, PasswordRegex);
+        }
+    }
+}
