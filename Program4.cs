7f21a5aaa7686f11d91efe2995ad3f5b58996c12
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 18:35:14 +0530

    User Registration

---
+++ Program4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssignmentDay24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Mobile Number ");
+            string mobileNumber = Console.ReadLine();
+            UC4 mobileRegex = new UC4();
+
+            if (mobileRegex.ValidMobileNumber(mobileNumber))
+                Console.WriteLine("Number is Correct");
+            else
+                Console.WriteLine("Number is Incorrect");
+        }
+    }
+}
---
+++ UC4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    class UC4
+    {
+        public static string NumberRegex = "^[91]{1}[ ]{1}[6-9]{1}[0-9]{9}$";
+
+        public bool ValidMobileNumber(string mobileNumber)
+        {
+            return Regex.IsMatch(mobileNumber, NumberRegex);
+        }
+    }
+}
+
