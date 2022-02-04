0e94a1ef89e6fe3d43af0be5f091cce1706f3393
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 18:43:56 +0530

    User Registration

---
+++ Program7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssignmentDay24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Password ");
+            string password = Console.ReadLine();
+            UC7 passwordRegex = new UC7();
+
+            if (passwordRegex.ValidPassword(password))
+                Console.WriteLine("Password is Correct");
+            else
+                Console.WriteLine("Password is Incorrect");
+        }
+    }
+}
---
+++ UC7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    class UC7
+    {
+        public static string PasswordRegex = "^(?=.*?[A-Z])(?=.*?[0-9])[A-Za-z0-9]{8,}$";
+
+        public bool ValidPassword(string password)
+        {
+            return Regex.IsMatch(password, PasswordRegex);
+        }
+    }
+}
