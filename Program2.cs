6bdb13bed1d9299edc6944e1b1ddbe869c1e9c63
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-04 18:30:14 +0530

    User Registration

---
+++ Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AssignmentDay24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Last Name");
+            string name = Console.ReadLine();
+
+            UC2 lastName = new UC2();
+
+            if (lastName.ValidLastName(name))
+               Console.WriteLine("Last Name is Correct");
+            else
+                Console.WriteLine("Last Name is Incorrect");
+
+        }
+    }
+}
---
+++ UC2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AssignmentDay24
+{
+    internal class UC2
+    {
+        public string NameRegex = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool ValidLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, NameRegex);
+        }
+    }
+}
+
